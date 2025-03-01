caac9b1b0745a2e768c41e8dedee4acdaa8b1520
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-01 19:59:01 +0300

    Update MainActivity.kt
    state yapısı

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +30,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Sayfa(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Sayfa(modifier=Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Sayfa(name: String, modifier: Modifier = Modifier) {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Anasayfa") }) }
-    ) {
-        paddingValues ->
-        Column (modifier=Modifier.fillMaxSize()
-            .padding(paddingValues)){
-            Text(text = "Merhaba Kullanıcı")
+fun Sayfa(modifier: Modifier=Modifier) {
+    val context= LocalContext.current
+    val sayac= remember { mutableStateOf(0) }
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text(text = "Sayaç: ${sayac.value}")
 
+        Button(onClick = {
+            sayac.value=10
+            Toast.makeText(context,"Aferin",Toast.LENGTH_LONG).show()
+        }) {
+            Text(text = "tıkla")
         }
     }
 }
@@ -52,6 +60,6 @@ fun Sayfa(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetpackComposeTheme {
-        Sayfa("Android")
+        Sayfa()
     }
 }
