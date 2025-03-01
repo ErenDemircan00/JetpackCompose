fe74f9d21c695c0fea25dc46bc2b8dc1a9f1cf27
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-01 19:24:01 +0300

    Güncelleme
    paddingvalues kullanımı

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Sayfa(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Sayfa(name: String, modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Anasayfa") }) }
+    ) {
+        paddingValues ->
+        Column (modifier=Modifier.fillMaxSize()
+            .padding(paddingValues)){
+            Text(text = "Merhaba Kullanıcı")
+
+        }
+        Column (modifier=Modifier.fillMaxSize()
+            .padding(paddingValues)
+            ){
+            Text(text = "merhaba")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+        Sayfa("Android")
+    }
+}
